2280f4a7ee9099a008e501e442aff5c3b8e7c575
Newms34 <newms3450@gmail.com>
2019-11-11 11:41:55 -0500

    moved attributes to scope object

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 It suggests things. Simply. 
 
 Don't worry! I'll add a *real* readme later!
+
+### TODO
+Add way to parse objects (arrays of objects) for item list?
--- js/index.js
+++ js/index.js
@@ -16,34 +16,150 @@ const app = angular
             "pizza",
             "hamburger",
             "cheeseburger"
-        ];
+        ].map(q=>({name:q,num:Math.ceil(Math.random()*15)}));
         $scope.derp = [];
         $scope.myFilter = (a, f) => {
-            console.log("filter", a, f);
+            // console.log("filter", a, f);
             if (!f) {
                 return (a && a.length && a) || [];
             }
-            return a.filter(q => q.toLowerCase().includes(f.toLowerCase()));
+            return a.filter(q => q.name.toLowerCase().includes(f.toLowerCase()));
         };
     })
     .directive("simpleAutoSuggest", function ($sce) {
         return {
             restrict: "E",
             scope: {
-                props: "="
+                items:'=simpleAutoItems',
+                subItem: '@simpleAutoSubItem',
+                filterFn:'=simpleAutoFilter',
+                adtnlClasses:'@simpleAutoClasses',
+                adtnlStyles:'@simpleAutoStyles',
+                nfText:'@simpleAutoNotFoundText',
+                nfFunc:'=simpleAutoNotFoundFn',
+                output:'=simpleAutoOutput',
             },
             template: `<div class='sas-cont'>
             <input class='sas-inp {{adtnlClasses}}' style='{{adtnlStyles}}' type='text' ng-model='filterSearch' ng-keyup='doFilter()' />
             <div style='width:{{inpBoxDims.width}}px; min-height:100px;' ng-show='hasFocus && ((filteredItems && filteredItems.length)||showNFBox)'>
-                    <div ng-repeat='si in filteredItems' class='simple-auto-suggest-suggestion' ng-click='pickItem(si);$event.stopPropagation();' ng-bind-html='hilite(si)'></div>
+                    <div ng-repeat='si in filteredItems' class='simple-auto-suggest-suggestion' ng-click='pickItem(si);$event.stopPropagation();' ng-bind-html='hilite(si,subItem)'></div>
                     <div ng-if='showNFBox'>
                         {{notFoundText}} <a href='#' ng-click='notFoundFunction(filterSearch)'>Click to add item</a>
                     </div>
                 </div>
         </div>`,
             link: function (scope, element, attributes) {
-                console.log("ATTRIBUTES", attributes, "ELEMENT", element);
-                scope.adtnlClasses = (attributes && attributes.simpleAutoClasses) || "";
+                console.log("ATTRIBUTES", attributes, "ELEMENT", element,'SCOPE',scope);
+                scope.hasFocus = true;
+                const inpBox = element[0].querySelector(".sas-inp");
+                setTimeout(function () {
+                    scope.inpBoxDims = element[0]
+                        .querySelector(".sas-inp")
+                        .getBoundingClientRect();
+                }, 1);
+                scope.focFunc = e =>{
+                    console.log('ng fokis',e)
+                }
+                scope.blurFunc = e =>{
+                    console.log('ng blerr',e)
+                }
+                scope.showNFBox = false;
+                scope.doFilter = () => {
+                    const filterOkay =
+                        scope.filterFn &&
+                        typeof scope.filterFn === "function" &&
+                        scope.items &&
+                        (scope.items instanceof Array);
+                    scope.filteredItems =
+                        (scope.filterSearch &&
+                            scope.filterSearch.length &&
+                            filterOkay &&
+                            scope.filterFn(scope.items, scope.filterSearch)) ||
+                        null;
+                    console.log(
+                        "FILTERED STOOF", scope.filteredItems,
+                        "SEARCH TERM",
+                        scope.filterSearch,
+                        'ALL ITEMS',
+                        scope.linkedItems,
+                        'Filter okay?', filterOkay
+                    );
+                    scope.showNFBox =
+                        !!scope.filterSearch &&
+                        (!scope.filteredItems || !scope.filteredItems.length);
+                        // scope.$digest();
+                }
+                scope.pickItem = it => {
+                    if(scope.output && scope.output!=='null'){
+                        //three different possibilities. First, if we're given a function, run dat function. Second, if it's an array, push into array. Thirdly, if neither, just replace
+                        if(typeof scope.output==='function'){
+                            console.log('output is fn!')
+                            scope.output(it);
+                        }else if(scope.output instanceof Array){
+                            console.log('output is array')
+                            scope.output.push(it);
+                        }else{
+                            scope.output=it;
+                        }
+                        scope.filterSearch = null;
+                    }else{
+                        scope.filterSearch = it;
+                    }
+                    scope.filteredItems = [];
+                    console.log('PARENT NOW',scope.$parent,scope.output)
+                };
+                console.log('THIS SAS IS',element[0].querySelector('.sas-cont'))
+                scope.hilite = t => {
+                    const termString = scope.subItem && t[scope.subItem]?t[scope.subItem]:typeof t!=='string'?JSON.stringify(t):t,
+                    pos = termString.indexOf(scope.filterSearch);
+                    return $sce.trustAsHtml(
+                        `${termString.slice(0, pos)}<strong>${scope.filterSearch}</strong>${termString.slice(
+                            scope.filterSearch.length + pos
+                        )}`
+                    );
+                };
+                element[0].querySelector('.sas-inp').addEventListener(
+                    "focus",
+                    function (e) {
+                        // console.log('focus event', e,this)
+                        scope.hasFocus = true;
+                        scope.$digest();
+                    },
+                    false
+                );
+                element[0].querySelector('.sas-inp').addEventListener(
+                    "blur",
+                    function (e) {
+                        setTimeout(function () {
+                            console.log('event',e,'new Focus',document.elementFromPoint(scope.x,scope.y))
+                            if(!document.elementFromPoint(scope.x,scope.y).className.includes('simple-auto-suggest-suggestion')){
+
+                                scope.hasFocus = false;
+                                scope.$digest();
+                            }
+                        }, 1);
+                    },
+                    false
+                );
+                element[0].addEventListener('mousemove',function simpMouse(e){
+                    // console.log('MOUSE POS (client)',e.clientX,e.clientY)
+                    scope.x = e.clientX;
+                    scope.y = e.clientY;
+                })
+            }
+        };
+    });
+/*
+<simple-auto-suggest simple-auto-items='myItems' simple-auto-filter='myFilter' simple-auto-classes='input is-half' simple-auto-styles='width:25%;' simple-auto-not-found-text='Cannot find item!' simple-auto-not-found-fn='null' simple-auto-output='derp'></simple-auto-suggest>
+    Item list: simple-auto-items
+    Filter Function: simple-auto-filter
+    CSS Classes: simple-auto-classes
+    In-line Styles: simple-auto-styles
+    Not Found text: simple-auto-not-found-text
+    Not Found function (run when no matches): simple-auto-not-found-fn
+    Output (function OR array OR string): simple-auto-output
+
+    scope.adtnlClasses = (attributes && attributes.simpleAutoClasses) || "";
                 scope.adtnlStyles = (attributes && attributes.simpleAutoStyles) || "";
                 scope.output = (attributes && attributes.simpleAutoOutput) || null;
                 scope.notFoundText = (attributes && attributes.simpleAutoNotFoundText) || "";
@@ -143,16 +259,4 @@ const app = angular
                     scope.x = e.clientX;
                     scope.y = e.clientY;
                 })
-            }
-        };
-    });
-/*
-<simple-auto-suggest simple-auto-items='myItems' simple-auto-filter='myFilter' simple-auto-classes='input is-half' simple-auto-styles='width:25%;' simple-auto-not-found-text='Cannot find item!' simple-auto-not-found-fn='null' simple-auto-output='derp'></simple-auto-suggest>
-    Item list: simple-auto-items
-    Filter Function: simple-auto-filter
-    CSS Classes: simple-auto-classes
-    In-line Styles: simple-auto-styles
-    Not Found text: simple-auto-not-found-text
-    Not Found function (run when no matches): simple-auto-not-found-fn
-    Output (function OR array OR string): simple-auto-output
 */
